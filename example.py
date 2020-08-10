01f78246a622a0bf8fed179efb95ea350c3a5c02
Chris Souers <csouers@gmail.com>
2020-08-10 12:14:28 -0400

    need some time between request and cancel. todo: check for response before continuing

--- example.py
+++ example.py
@@ -5,11 +5,10 @@ import time
 from panda import Panda
 from honda import HondaInputOutputController
 
-BUS = 0 # must be B-CAN bus for Honda
+BUS = 1 # must be B-CAN bus for Honda
 
 p = Panda()
 ioc = HondaInputOutputController(lambda addr, dat: p.can_send(addr, dat, BUS))
-
 print("lock all doors ...")
 ioc.lock_all_doors()
 time.sleep(1)
@@ -20,14 +19,17 @@ print("right turn signal ...")
 ioc.right_turn_signal(True)
 time.sleep(1)
 ioc.right_turn_signal(False)
+time.sleep(0.2)
 print("left turn signal ...")
 ioc.left_turn_signal(True)
 time.sleep(1)
 ioc.left_turn_signal(False)
+time.sleep(0.2)
 print("headlight low beams ...")
 ioc.headlight_low_beams(True)
 time.sleep(1)
 ioc.headlight_low_beams(False)
+time.sleep(0.2)
 print("headlight high beams ...")
 ioc.headlight_high_beams(True)
 time.sleep(1)
